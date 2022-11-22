70ba0afefa05b4ae05be86b0c00bd928c5df037b
hanad isse <hanadisse1@gmail.com>
2022-11-22 10:45:00 -0600

    added comments to my code

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 var save = $(".saveBtn")
 
 function dayPlanner() {
+    // gets the current date
     var today = moment().format("dddd MMMM Do")
     $("#currentDay").text(today)
 
+    // gets the current hour
     var currentTime = moment().hours()
+    // grabs each time-block class
     var timeBlock = $(".time-block")
+    // loops through all time-block classes
     timeBlock.each(function() {
+        // splits each hour into its own item
         var hour = parseInt($(this).attr("id"))
+        // grabs the id to that div, compares current hour to that divs id value
         if(hour === currentTime) {
             $(this).children(".col-sm-10").attr("class", "present col-sm-10 description")
         } else if (hour < currentTime) {
